4dc9801d86451ea285e386ed05fc35d77fd3db4f
attoumbre <juvenalak@gmail.com>
2022-01-06 15:23:01 +0100

    first commit

---
+++ src/test/java/fr/istic/miage/m1/juvenal/FirstPdf/AppTest.java
@@ -0,0 +1,28 @@
+package fr.istic.miage.m1.juvenal.FirstPdf;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	
+        assertTrue( true );
+    }
+    
+    @Test
+    public void addMetaData() {
+    	assertFalse(false);
+    }
+}
